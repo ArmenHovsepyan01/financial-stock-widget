aeba45e662a217af771d3574b24d19615fcd3253
ArmenHovsepyan01 <arh20025@gmail.com>
2024-03-29 17:25:49 +0400

    fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Layout from './layout/layout.tsx';
 import { Provider } from 'react-redux';
 import store from './redux/store/store.ts';
 import Widgets from './components/widgets/Widgets.tsx';
-import Alert from './components/alert/Alert.tsx';
 
 const App = () => {
   return (
